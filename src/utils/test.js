258c7e81cf2517ab160883a68f35b7d86645a8ad
Abdellah Alaoui <als.abdellah@gmail.com>
2017-10-16 14:49:59 +0100

    add tests for stopwatch:
    - must accept numeric elapsedMilliseconds as its only argument
    - correctly formats time less than a second
    - correctly formats time less than a minute
    - correctly formats time less than an hour
    - correctly formats time more than one hour

---
+++ src/utils/test.js
@@ -0,0 +1,29 @@
+//@flow
+import { formatElapsedTime } from "./";
+describe("formatElapsedTime", () => {
+  it("formatElapsedTime() must accept numeric elapsedMilliseconds as its only argument", () => {
+    let input = "three";
+    let expectedOutput = "00:00:00";
+    expect(formatElapsedTime(input)).toEqual(expectedOutput);
+  });
+  it("formatElapsedTime() correctly formats time less than a second", () => {
+    let input = 300;
+    let expectedOutput = "00.30";
+    expect(formatElapsedTime(input)).toEqual(expectedOutput);
+  });
+  it("formatElapsedTime() correctly formats time less than a minute", () => {
+    let input = 2500;
+    let expectedOutput = "02.50";
+    expect(formatElapsedTime(input)).toEqual(expectedOutput);
+  });
+  it("formatElapsedTime() correctly formats time less than an hour", () => {
+    let input = 129100;
+    let expectedOutput = "02:09.10";
+    expect(formatElapsedTime(input)).toEqual(expectedOutput);
+  });
+  it("formatElapsedTime() correctly formats time more than one hour", () => {
+    let input = 100000000;
+    let expectedOutput = "27:46:40.00";
+    expect(formatElapsedTime(input)).toEqual(expectedOutput);
+  });
+});
