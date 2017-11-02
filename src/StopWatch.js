d5beeeb56c1d4ca342d576bc57c38156c8fec2d3
Abdellah Alaoui <als.abdellah@gmail.com>
2017-11-02 09:37:09 +0000

    fix homepage and remove eslint warnings

--- src/StopWatch.js
+++ src/StopWatch.js
@@ -1,9 +1,9 @@
 //@flow
 import React from "react";
-import { formatElapsedTime, twoDigits } from "./utils";
+import { formatElapsedTime } from "./utils";
 import "./StopWatch.css";
 type Props = {
-  tick: number,
+  tick: number
 };
 type State = {
   laps: Array<number>,
@@ -14,7 +14,7 @@ class StopWatch extends React.Component<Props, State> {
   initialState = {
     isStopped: false,
     laps: [],
-    currentLapElapsedTime: 0,
+    currentLapElapsedTime: 0
   };
   constructor(props: Props) {
     super(props);
@@ -26,32 +26,32 @@ class StopWatch extends React.Component<Props, State> {
     this.resume = this.resume.bind(this);
     this.reset = this.reset.bind(this);
     this.renderControlBar = this.renderControlBar.bind(this);
-    this.intervalId = null
+    this.intervalId = null;
   }
   start = () => {
     this.intervalId = setInterval(() => {
       this.setState((prevState, props) => ({
         ...prevState,
-        currentLapElapsedTime: prevState.currentLapElapsedTime + props.tick,
+        currentLapElapsedTime: prevState.currentLapElapsedTime + props.tick
       }));
     }, this.props.tick);
 
     this.setState(prevState => ({
       ...prevState,
-      isStopped: false,
+      isStopped: false
     }));
   };
   stop = () => {
     this.intervalId !== null && clearInterval(this.intervalId);
     this.setState((prevState, props) => ({
-      isStopped: true,
+      isStopped: true
     }));
   };
   lap = () => {
     this.setState((prevState, props) => ({
       ...prevState,
       laps: [prevState.currentLapElapsedTime, ...prevState.laps],
-      currentLapElapsedTime: 0,
+      currentLapElapsedTime: 0
     }));
   };
   resume = () => {
@@ -68,28 +68,29 @@ class StopWatch extends React.Component<Props, State> {
     } else if (!isStopped) {
       return [
         <button onClick={this.stop}>Stop</button>,
-        <button onClick={this.lap}>Lap</button>,
+        <button onClick={this.lap}>Lap</button>
       ];
     } else {
       return [
         <button onClick={this.resume}>Resume</button>,
-        <button onClick={this.reset}>Reset</button>,
+        <button onClick={this.reset}>Reset</button>
       ];
     }
   };
   render() {
     const { laps, currentLapElapsedTime } = this.state;
-    const totalElapsedTime = laps.reduce((a, b) => a + b, 0) + currentLapElapsedTime;
+    const totalElapsedTime =
+      laps.reduce((a, b) => a + b, 0) + currentLapElapsedTime;
     return (
       <div className="container">
         <h1>{formatElapsedTime(totalElapsedTime)}</h1>
         <h3>{formatElapsedTime(currentLapElapsedTime)}</h3>
         <table className="summary-table">
           {laps.map((lap, index) => {
-            const i = laps.length - index - 1
+            const i = laps.length - index - 1;
             const aggregatedElapsedTime = laps
               .slice(0, i + 1)
-              .reduce((a, b) => a + b, 0)
+              .reduce((a, b) => a + b, 0);
             return (
               <tr key={i}>
                 <td>{i}</td>
@@ -106,6 +107,6 @@ class StopWatch extends React.Component<Props, State> {
 }
 //$FlowFixMe
 StopWatch.defaultProps = {
-  tick: 10,
+  tick: 10
 };
 export default StopWatch;
--- src/utils/index.js
+++ src/utils/index.js
@@ -15,20 +15,15 @@ const formatElapsedTime = (elapsedMilliseconds: number): string => {
   ) {
     return "00:00.00";
   } else {
-    let milliseconds = parseInt((elapsedMilliseconds % 1000) / 10);
-    let hours = parseInt(elapsedMilliseconds / 3600000);
-    let minutes = parseInt((elapsedMilliseconds / 60000) % 60);
-    let seconds = parseInt((elapsedMilliseconds / 1000) % 60);
-    // let seconds = (elapsedMilliseconds - milliseconds) %
-    // , seconds, minutes, hours;
+    let milliseconds = parseInt(elapsedMilliseconds % 1000 / 10, 10);
+    let hours = parseInt(elapsedMilliseconds / 3600000, 10);
+    let minutes = parseInt(elapsedMilliseconds / 60000 % 60, 10);
+    let seconds = parseInt(elapsedMilliseconds / 1000 % 60, 10);
+
     if (hours) {
-      return `${twoDigits(hours)}:${twoDigits(minutes)}:${twoDigits(
-        seconds,
-      )}.${twoDigits(milliseconds)}`;
+      return `${twoDigits(hours)}:${twoDigits(minutes)}:${twoDigits(seconds)}.${twoDigits(milliseconds)}`;
     } else {
-      return `${twoDigits(minutes || 0)}:${twoDigits(seconds)}.${twoDigits(
-        milliseconds,
-      )}`;
+      return `${twoDigits(minutes)}:${twoDigits(seconds)}.${twoDigits(milliseconds)}`;
     }
   }
 };
